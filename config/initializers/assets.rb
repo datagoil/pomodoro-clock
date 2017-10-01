9b5952b4f52df0b7db4b0b0a6e0503d429a073dd
Sara S <ida.azalia@protonmail.com>
2017-10-01 00:18:22 -0500

    JS and CSS added

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
-
+Rails.application.config.assets.precompile += %w( main.js )
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-
+Rails.application.config.assets.precompile += %w( clock.scss )
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.

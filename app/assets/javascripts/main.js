2a1ee7b20efed6574588240ad987c61b0d078115
Sara S <ida.azalia@protonmail.com>
2017-10-01 01:27:15 -0500

    added sample countdown and start of table

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,50 +1,22 @@
-var x = "";
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
-function makeGrid(x){
-    for (rows=0; rows < x; rows++) {
-        for (columns=0; columns < x; columns++) {
-            $('.container').append('<div class = "grid"></div>');
-        };
-    };
-    $('.grid').width(960/x);
-    $('.grid').height(960/x);
-};
-function randomColors() {
-  var c = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += c[Math.floor(Math.random() * 16)];
-  };
-  return color;
-};
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
-function mousey() {
-    $('.grid').mouseenter(function(){
-          $(this).css("background-color", randomColors());
-    });
-    $('.grid').click(function() {
-        $(this).css("background", "none");
-    });
- };
+        display.text(minutes + ":" + seconds);
 
-function clearGrid() {
-   $('.grid').css("background", "none");
-   makeGrid(0);
-};
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
 
-$(document).ready(function() {
-     makeGrid(16);
-     mousey();
-     $('button').click(function (){
-        clearGrid();
-        var gridInput = prompt('Please enter the number of rows for your grid');
-            if (gridInput === null || gridInput === '') {
-                makeGrid(16);
-            } else if (isNaN(gridInput)) {
-                makeGrid(16);
-            } else {
-            makeGrid(gridInput);
-            mousey();
-            };
-     });
+jQuery(function ($) {
+    var fiveMinutes = 60 * 5,
+        display = $('#time');
+    startTimer(fiveMinutes, display);
 });
